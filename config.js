305ba3edfd08be8c12efff3b0dfb9e82b8a3389c
Denis Forigo <denisforigo@gmail.com>
2018-08-07 10:37:10 -0300

    model_config.js added

--- config.js
+++
@@ -1,4 +0,0 @@
-export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const url = 'http://eupassarinho.org/';
-export const count = 100;
-export const path = "poesiadeluta";
---
+++ config_model.js
@@ -0,0 +1,6 @@
+//Create a config like this:
+
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const url = "http://eupassarinho.org/";
+export const count = 100;
+export const path = "poesiadeluta";
